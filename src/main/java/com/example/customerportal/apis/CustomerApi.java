d365e18f57237dcc992155188f70adab7c27e07a
Vivekanand P V <vivekanandpv@outlook.com>
2022-07-22 16:21:02 +0530

    cross origin

--- src/main/java/com/example/customerportal/apis/CustomerApi.java
+++ src/main/java/com/example/customerportal/apis/CustomerApi.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/customers")
+@CrossOrigin
 public class CustomerApi {
     private final IServerResponseFactory responseFactory;
     private final CustomerService customerService;
